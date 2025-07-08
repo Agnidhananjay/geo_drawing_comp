d442265988730e9a8367cddd23b8c8645fcfdb69
MrFluorine <agnidhananjay@gmail.com>
2025-07-08 09:47:58 +0530

    changed model

--- test.py
+++ test.py
@@ -83,7 +83,7 @@ def encode_image(image):
 def compare_drawings(previous_image_base64, current_image_base64):
     try:
         response = client.chat.completions.create(
-            model="gpt-4o",
+            model="gpt-o3",
             messages=[
                 {
                     "role": "user",
