7c001896e26f9c22054be3b0efc442c9142d1d31
MrFluorine <agnidhananjay@gmail.com>
2025-07-10 11:40:10 +0530

    added o3-pro

--- test.py
+++ test.py
@@ -83,7 +83,7 @@ def encode_image(image):
 def compare_drawings(previous_image_base64, current_image_base64):
     try:
         response = client.chat.completions.create(
-            model="o3-2025-04-16",
+            model="o3-pro-2025-06-10",
             messages=[
                 {
                     "role": "user",
